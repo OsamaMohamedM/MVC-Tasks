4491d348ae7298512649038a0ebd749d9bff421d
osama <osamamoahmed242@gmail.com>
2026-02-18 00:32:39 +0200

    feat: Create PipelineGurdian project add CustomMiddleWares

---
+++ PipelineGuardian/PipelineGuardian/Middleware/MaintenanceMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace PipelineGuardian.Middleware
+{
+    public class MaintenanceMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public MaintenanceMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var isMaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode");
+            if (isMaintenanceMode)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("The service is currently under maintenance. Please try again later.");
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
